a8e5def625abd66b9147bf160b24ef0ca9a00116
Awodire babajide samuel <jidex199@gmail.com>
2021-02-14 14:27:03 +0100

    Finally setup all the required UI requirements.

---
+++ DroAssessmentApp/src/main/java/rhymezxcode/droassessment/Activities/product_details.kt
@@ -0,0 +1,12 @@
+package rhymezxcode.droassessment.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import rhymezxcode.droassessment.R
+
+class product_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
--- DroAssessmentApp/src/main/java/rhymezxcode/droassessment/Activities/view_products.kt
+++ DroAssessmentApp/src/main/java/rhymezxcode/droassessment/Activities/view_products.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import rhymezxcode.droassessment.R
 
-class ViewProductsActivity : AppCompatActivity() {
+class view_products : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_products)
+        setContentView(R.layout.activity_view_products)
     }
 }
